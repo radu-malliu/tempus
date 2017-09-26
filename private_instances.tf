2deb2c5439335218e9676f156524af06d96b0d89
Radu <radu_malliu@yahoo.com>
2017-09-25 20:48:00 -0500

    Remove var from count for now

--- private_instances.tf
+++ private_instances.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-9eb4b1e5"
   instance_type = "t2.micro"
-	count = "${var.count}"
+	count = "3"
 
-  depends_on = ["aws_s3_bucket.example"]
+  depends_on = ["aws_subnet.private"]
 }
