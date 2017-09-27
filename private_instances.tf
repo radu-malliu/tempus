bb1e7d210b2e8e60593fc29cdba144dece44df7f
Radu <radu_malliu@yahoo.com>
2017-09-27 15:34:54 -0500

    confirmed working set, pending comments

--- private_instances.tf
+++ private_instances.tf
@@ -5,16 +5,18 @@ variable "inst_count" {
 resource "aws_instance" "example" {
   count = "${var.inst_count}"
 	
-	ami           = "ami-9eb4b1e5"
+	ami           = "${lookup(var.amis, var.region_name)}"
   instance_type = "t2.micro"
 	subnet_id = "${aws_subnet.private.id}"
 	key_name = "${aws_key_pair.to_docker.key_name}"
 	vpc_security_group_ids = ["${aws_security_group.private.id}"]
 
-  depends_on = ["aws_subnet.private"]
+  depends_on = ["aws_subnet.private", "aws_nat_gateway.ngw"]
 
 	provisioner "remote-exec" {
     inline = [
+			"sudo yum update -y",
+			"sudo yum install -y docker",
       "sudo service docker start",
 			"sudo docker run hello-world"
     ]
@@ -23,6 +25,7 @@ resource "aws_instance" "example" {
     	
 			user = "ec2-user"
 			private_key = "${file("to_docker")}"
+			agent = "false"
 
 			bastion_host = "${aws_instance.bastion.public_ip}"
 			bastion_user = "ec2-user"
--- public_instance.tf
+++ public_instance.tf
@@ -1,8 +1,26 @@
-#TODO: add map of amis per region
+variable "amis" {
+	default = {
+		"us-east-1" = "ami-a4c7edb2"
+  	"us-east-2" = "ami-8a7859ef"
+		"us-west-1" = "ami-327f5352"
+		"us-west-2" = "ami-6df1e514"
+		"ca-central-1" = "ami-a7aa15c3"
+		"ap-northeast-1" = "ami-3bd3c45c"
+		"ap-northeast-2" = "ami-e21cc38c"
+		"ap-southeast-1" = "ami-77af2014"
+		"ap-southeast-2" = "ami-10918173"
+		"ap-south-1" = "ami-47205e28"
+		"cn-northwest-1" = "ami-b6f928db"
+		"eu-central-1" = "ami-82be18ed"
+		"eu-west-1" = "ami-d7b9a2b1"
+		"eu-west-2" = "ami-ed100689"
+		"sa-east-1" = "ami-87dab1eb"
+	}
+}
 
 resource "aws_instance" "bastion" {
 	
-	ami           = "ami-a4c7edb2"
+	ami           = "${lookup(var.amis, var.region_name)}"
   instance_type = "t2.micro"
 	subnet_id = "${aws_subnet.public.id}"
 	key_name = "${aws_key_pair.to_bastion.key_name}"
@@ -18,6 +36,7 @@ resource "aws_instance" "bastion" {
     	type     = "ssh"
     	user     = "ec2-user"
     	private_key = "${file("to_bastion")}"
+			agent = "false"
   	}
 	}
 
@@ -29,6 +48,7 @@ resource "aws_instance" "bastion" {
       type     = "ssh"
       user     = "ec2-user"
       private_key = "${file("to_bastion")}"
+			agent = "false"
     }
 	}
 }
